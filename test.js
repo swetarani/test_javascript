dbc5dbde65b9daba49fa326e7567bab1f0793600
Sweta Rani <swetarani3007@gmail.com>
2015-06-24 13:36:34 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-<script>
-	var settings = {
-	    "async": true,
-		"crossDomain": true,
-		"url": "https://test-store-1017.myshopify.com/admin/themes/33274243/assets.json",
-		"method": "PUT",
-		"headers": {
-			"content-type": "application/json",
-			"x-shopify-access-token": "480f6d63072def2fb625e753e116a034"
-		},
-		"processData": false,
-		"data": "{\r\n    \"asset\": {\r\n    \"key\": \"layout\\/alternate.liquid\",\r\n    \"source_key\": \"layout\\/theme.liquid\"\r\n        \r\n    }\r\n}"
-	}
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://test-store-1017.myshopify.com/admin/script_tags.json",
+	"method": "PUT",
+	"headers": {
+		"content-type": "application/json",
+		"x-shopify-access-token": "480f6d63072def2fb625e753e116a034"
+		
+	},
+	"processData": false,
+	"data": "{\r\n    \"script_tag\": {\r\n    \"event\": \"onload\",\r\n    \"src\": \"https://raw.githubusercontent.com/swetarani/test_javascript/master/test.js\"\r\n        \r\n    }\r\n}"
 	
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-	});
-</script>
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
