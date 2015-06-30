11bb7040c45fe9293192e4a58bc6fbc4845afc26
Sweta Rani <swetarani3007@gmail.com>
2015-06-30 17:19:42 +0530

    Update q.js

--- q.js
+++ q.js
@@ -1,7 +1,17 @@
 alert ("hello how are you");
 var push_checkout = function(){
-	var len = Shopify.checkout.line_items.length
-	wizrocket.event=[]
+	var len = Shopify.checkout.line_items.length;
+	var items = [];
+	for (i=0; i<len; i++) {
+		var obj = {};
+	 	//alert(Shopify.checkout.line_items[i].title);
+	 	obj["product_id"] = Shopify.checkout.line_item[i].product_id;
+	 	obj["title"] = Shopify.checkout.line_items[i].title;
+	 	obj["quantity"] = Shopify.checkout.line_items[i].quantity;
+	 	obj["vendor"] = Shopify.checkout.line_items[i].vendor;
+	 	items.push(obj);
+ 	}
+	wizrocket.event=[];
  	wizrocket.event.push("Chechout",{
  		"Amount" : Shopify.checkout.total_price,
  		"Currency" : Shopify.checkout.currency,
@@ -9,23 +19,17 @@ var push_checkout = function(){
  		"Ship_region" : Shopify.checkout.shipping_address.province,
  		"Ship_city" : Shopify.checkout.shipping_address.city,
  		"email" : Shopify.checkout.email,
- 		"items" : 
- 		for (i=0; i<len; i++) {
- 			//alert(Shopify.checkout.line_items[i].title);
- 			"product_id" : Shopify.checkout.line_item[i].product_id,
- 			"title" : Shopify.checkout.line_items[i].title,
- 			"quantity" : Shopify.checkout.line_items[i].quantity,
- 			"vendor" : Shopify.checkout.line_items[i].vendor
- 		}
+ 		"items" : items
+ 		
 // Payment mode (Credit Card, Paypal)
 // Items (see the platform specific integration guide for sending a list of products sold)
  	})
  	alert ("hello checkout");
-	alert(JSON.stringify(wizrocket.event))
+	alert(JSON.stringify(wizrocket.event));
 }
 var ref = window.location.href;
 var res = ref.match(/thank_you/gi);
-alert (res)
+alert (res);
 if (res != null){
 	alert ("its thank_you page");
 	push_checkout();
