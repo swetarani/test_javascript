5fe9771d6ff5c814fb2307d6490e6d2b7ed40ee2
Sweta Rani <swetarani3007@gmail.com>
2015-06-30 16:14:33 +0530

    Update q.js

--- q.js
+++ q.js
@@ -1,1 +1,17 @@
 alert ("hello how are you");
+var push_checkout = function(){
+	wizrocket.event=[]
+ 	wizrocket.event.push("Chechout",{
+ 		"Amount" : Shopify.checkout.payment_due
+//  		Amount (150, 499)
+// Currency (USD, INR)
+// Payment mode (Credit Card, Paypal)
+// Items (see the platform specific integration guide for sending a list of products sold)
+// Ship country (US, India)
+// Ship region (California, Maharashtra)
+// Ship city (San Francisco, Mumbai)
+ 	})
+ 	alert ("hello checkout");
+	alert(JSON.stringify(wizrocket.event))
+}
+document.getElementsByClassName("thank-you") = push_checkout
