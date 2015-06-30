6fb543aa0d798cde728bd5d59416d6594d16e848
Sweta Rani <swetarani3007@gmail.com>
2015-06-30 16:00:37 +0530

    Create a.js

---
+++ a.js
@@ -0,0 +1,10 @@
+var wizrocket = {event:[], profile:[], account:[], enum:function(e){return '$E_' + e}};
+wizrocket.account.push({"id": "WWW-WWW-WWWZ"});
+(function () {
+    var wzrk = document.createElement('script');
+	wzrk.type = 'text/javascript';
+	wzrk.async = true;
+	wzrk.src = ('https:' == document.location.protocol ? 'https://d2r1yp2w7bby2u.cloudfront.net' : 'http://static.wizrocket.com') + '/js/a.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wzrk, s);
+})();
